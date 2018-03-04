b072fea5235cfe774ff04dcb2c39aae67440584c
Yulia Sobol <ysobol@provectus.com>
2018-03-04 10:13:16 +0200

    other css changes

--- sources/index.js
+++ sources/index.js
@@ -1,3 +1,12 @@
-console.log('in index.js');
-console.log($);
-console.log(jQuery);
+(function() {
+    showMenu = () => {
+        const isHiddentClass = 'isHidden';
+        const classList = document.querySelector('.nav').classList;
+
+        if (classList.contains(isHiddentClass)) {
+            classList.remove(isHiddentClass);
+        } else {
+            classList.add(isHiddentClass);
+        }
+    };
+}());
