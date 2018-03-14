86f2c27c88e3f5779fa597b96517cad9994ef3bb
Yulia Sobol <ysobol@provectus.com>
2018-03-14 16:19:22 +0200

    some ui changes according to comment

--- sources/index.js
+++ sources/index.js
@@ -1,7 +1,7 @@
 (function() {
     showMenu = () => {
         const isHiddentClass = 'isHidden';
-        const classList = document.querySelector('.nav').classList;
+        const classList = document.querySelector('nav').classList;
 
         if (classList.contains(isHiddentClass)) {
             classList.remove(isHiddentClass);
